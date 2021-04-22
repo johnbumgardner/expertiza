f34bbf15cf7574cb197dbb5bc04b19ad7dd975be
John Bumgardner <john.bumgardner@icloud.com>
2021-04-22 12:21:12 -0400

    change table migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210422145525) do
+ActiveRecord::Schema.define(version: 20210422161349) do
 
   create_table "account_requests", force: :cascade do |t|
     t.string   "name",              limit: 255
