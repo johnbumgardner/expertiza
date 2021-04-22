df66f2602bc69566ba70f5d2ce6eca35524d21ea
John Bumgardner <john.bumgardner@icloud.com>
2021-04-22 10:54:41 -0400

    change the migration version number

--- app/models/quality_filtering.rb
+++ app/models/quality_filtering.rb
@@ -5,11 +5,12 @@ class QualityFiltering
 		# Filters is a list of filters to apply, so [:threshold_filter, :outlier_filter]
 		filters.each do |filter|
 			# call appropriate filtering method if the filter in the static maintainable list
-			answers = self.multiplex_to_filter(filter) if FILTERING_METHODS.include? filter
-		end
+			answers = self.multiplex_to_filter(answers, filter) if FILTERING_METHODS.include? filter
+		end 
 		answers
 	end
 
+	# Based on desired filters, call the specific filter function
 	def multiplex_to_filter(answers, filter)
 		case filter
 		when :threshold_filter
@@ -21,9 +22,9 @@ class QualityFiltering
 		answers
 	end
 
+	# Filter out all answers with comment length less than a set threshold
 	def apply_threshold_filter(answers, threshold)
 		answers = answers.select{|answer| answer.comments.split(' ').length > threshold}
-		
 	end
 
 	def apply_outlier_filter(answers) 
