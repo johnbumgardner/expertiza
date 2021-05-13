3ba28a506744795a8d26d76c88191db6b5f3099c
Sichao25 <54721834+Sichao25@users.noreply.github.com>
2021-05-13 17:04:39 -0400

    Filtering (#5)
    * Rubric test (#1949)
    
    * move code changes for account name in email to master
    
    * fix code climate issues in users_controller_spec.rb
    
    * fix final code climate issue
    
    * create test
    
    * remove old test
    
    * fix code climate
    
    * fix parenthesis
    
    * remove function that isnt ever used and cannot be
    
    * fix code climate
    
    * add parameters
    
    * fix functin
    
    * update README.md to include link to Development Discord
    
    * create quality filtering model
    
    * add threshold
    
    Co-authored-by: John Bumgardner <38589044+johnbumgardner@users.noreply.github.com>

--- app/models/quality_filtering.rb
+++ app/models/quality_filtering.rb
@@ -17,7 +17,8 @@ class QualityFiltering < ActiveRecord::Base
 			threshold = tag_prompt_deployment.answer_length_threshold
 			answers = self.apply_threshold_filter(answers, threshold)
 		when :outlier_filter
-			answers = self.apply_outlier_filter(answers)
+      threshold = 5
+			answers = self.apply_outlier_filter(answers, threshold)
 		end
 		answers
 	end
@@ -27,8 +28,38 @@ class QualityFiltering < ActiveRecord::Base
 		answers = answers.select{|answer| answer.comments.split(' ').length > threshold}
 	end
 
-	def apply_outlier_filter(answers) 
-		# For Sichao to implement
+  # Filter out all answers which is the outlier in their group
+	def apply_outlier_filter(answers, threshold) 
+    tag_prompt_deployments = TagPromptDeployment.where(id: self.tag_prompt_deployment_id)
+    teams = Team.where(parent_id: tag_prompt_deployments.assignment_id)
+    questions = Question.where(questionnaire_id: tag_prompt_deployments.questionnaire.id, type: tag_prompt_deployments.question_type)
+    outlier_user_ids = []
+    unless teams.empty? or questions.empty?
+      teams.each do |team|
+        users = TeamsUser.where(team_id: team.id).map(&:user)
+        users.each do |user|
+          tags = AnswerTag.where(tag_prompt_deployment_id: self.id, user_id: user.id, answer_id: answers.map(&:id))
+
+          outliers = 0
+          questions.each do |question|
+            answer = answers.where(question_id: question.id)
+            user_tag = tags.where(answer_id: answer.map(&:id))
+            teammates = users.select {|e| e != user}
+            unless teammates.empty?
+              teammates_tags = AnswerTag.where(tag_prompt_deployment_id: self.id, user_id: teammates.map(&:id), answer_id: answer.map(&:id))
+              if teammates_tags.uniq.size <= 1 && teammates_tags[0] != user_tag
+                outliers += 1
+              end
+            end
+          end
+          if outliers > threshold
+            outlier_user_ids.append(user.id)
+          end
+        end
+      end
+    end
+    answer_tags = tags.where{|answer_tag| answer_tag.user_id != outlier_user_ids}
+    answers = answers.select{|answer| answer.id != answer_tags.answer_id}
 		answers
 	end
 end
--- db/migrate/137_create_quality_filterings.rb
+++ db/migrate/137_create_quality_filterings.rb
@@ -1,10 +1,10 @@
 class CreateQualityFilterings < ActiveRecord::Migration
   def self.up
     create_table "quality_filtering", :force => true do |t|
-      
+
     end
   end
-  
+
   def self.down
   end
 end
